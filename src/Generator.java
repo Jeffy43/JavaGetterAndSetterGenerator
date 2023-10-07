1faf19fa2c0044852fe642a40dbc27bbd8600bb8
Jeffy <jeffyp@nycstudents.net>
2023-10-07 17:04:01 -0400

    Update

--- src/Generator.java
+++ src/Generator.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Generator {
+import java.util.Scanner;
+public class Generator {
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Welcome to the AP Java getter and setter code generator!\nEnter your instance variable name:");
+        String name = scan.nextLine();
+        System.out.println("Enter the type for the variable size: ");
+        String type = scan.nextLine();
+        String upFirst = name.substring(0, 1).toUpperCase();
+        String restOfName = name.substring(1);
+        String upName = upFirst + restOfName;
+        System.out.println("//getter for " + name + "()\npublic " + type + " get" + upName + "()\n{\n return " + name + ";\n}" );
+        System.out.println("//setter for " + name + "()\npublic void set" + upName + "(" + type + " " + name + ")\n{\n this." + name + " = " + name + "\n}");
+    }
 }
